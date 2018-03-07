ac351c822d368bb9bbd87f8403b840daacee793d
Szekely Istvan <steven.sekler@gmail.com>
2018-03-07 18:58:41 +0200

    feat: Database helper class added

---
+++ app/src/main/java/com/example/szekelyistvan/popularmovies/Utils/FavouritesDbHelper.java
@@ -0,0 +1,32 @@
+package com.example.szekelyistvan.popularmovies.Utils;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static com.example.szekelyistvan.popularmovies.Utils.FavouritesContract.FavouritesEntry.CREATE_TABLE;
+import static com.example.szekelyistvan.popularmovies.Utils.FavouritesContract.FavouritesEntry.DB_NAME;
+import static com.example.szekelyistvan.popularmovies.Utils.FavouritesContract.FavouritesEntry.DB_VERSION;
+import static com.example.szekelyistvan.popularmovies.Utils.FavouritesContract.FavouritesEntry.TABLE_NAME;
+
+/**
+ * Created by Szekely Istvan on 07.03.2018.
+ */
+
+public class FavouritesDbHelper extends SQLiteOpenHelper{
+
+    public FavouritesDbHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
